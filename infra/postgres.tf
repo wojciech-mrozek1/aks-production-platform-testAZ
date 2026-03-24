455789ff3cae70b26d508e0e610e52386f50bfdb
Wojciech Mrozek <wojciech.mrozek@dxc.com>
2026-03-24 02:14:51 -0700

    add terraform infra and github actions workflow

--- infra/postgres.tf
+++ infra/postgres.tf
@@ -10,7 +10,7 @@ resource "azurerm_postgresql_flexible_server" "this" {
   delegated_subnet_id = azurerm_subnet.postgres.id
   private_dns_zone_id = azurerm_private_dns_zone.postgres.id
 
-  public_network_access_enabled = false  # 👈 KLUCZOWE
+  public_network_access_enabled = false
 
   zone                  = "1"
   storage_mb            = var.postgres_storage_mb
@@ -22,4 +22,11 @@ resource "azurerm_postgresql_flexible_server" "this" {
   ]
 
   tags = local.common_tags
+}
+
+resource "azurerm_postgresql_flexible_server_database" "app" {
+  name      = "appdb"
+  server_id = azurerm_postgresql_flexible_server.this.id
+  collation = "en_US.utf8"
+  charset   = "UTF8"
 }
